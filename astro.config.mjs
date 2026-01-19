41bdb1cbfa474fa35b5b85829c058629bd2c662b
t-aono <46856574+t-aono@users.noreply.github.com>
2026-01-19 10:36:50 +0900

    github pages設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://t-aono.github.io',
+  base: '/tda',
   vite: {
     plugins: [tailwindcss()]
   }
